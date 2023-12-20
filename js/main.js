84d81f8820c0c9b8d613af31b7f9c79decbcf867
Moataz Mohammed <116386227+m0atazm0hammed@users.noreply.github.com>
2023-12-20 05:25:10 +0200

    logo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 function formatDate(date) {
+    // Define the formatting options for the date
     const options = { weekday: 'long', day: 'numeric', month: 'long' };
+
+    // Create a new Date object using the input date
+    // and format it as a string using the toLocaleDateString method
     return new Date(date).toLocaleDateString('en-US', options);
 }
+
 
 // Get the current date
 const currentDate = new Date();
@@ -9,5 +14,5 @@ const currentDate = new Date();
 // Format the date
 const formattedDate = formatDate(currentDate);
 
-// Display the formatted date in the HTML element with id "dateContainer"
+// Display the formatted date in the HTML element with id "main-container-title-h2"
 document.getElementById('main-container-title-h2').appendChild(document.createTextNode(formattedDate));
